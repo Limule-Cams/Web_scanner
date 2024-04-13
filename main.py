0da0fdef910ba2e0f8d19ed322d9abfe8c8a5b31
Limule-Cams <hcamara903@gmail.com>
2024-04-13 23:37:35 +0000

    c

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 
 import mechanize
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
-class visite(object):
+class Visite(object):
 
     def __init__(self, url, user_agent, proxy=None) -> None:
         if url.endswith("/"):
@@ -13,6 +14,7 @@ class visite(object):
              self.agent = user_agent
              self.br = mechanize.Browser()      
              self.all_link = []
+             self.url = url
 
     def view_page(self, url=None):
         if url is None:
@@ -32,9 +34,38 @@ class visite(object):
               print(i)
     
 
-    def get_link(self, url):
+    def get_link(self, url=None):
+        list_link = []
         if url is None:
              url = self.url
         page_source = self.view_page(url)
         if page_source is not None:
-             soup = BeautifulSoup(page_source, "html_parser")
+             soup = BeautifulSoup(page_source, "html.parser")
+             link_parse = urlparse(url)
+             for l in soup.find_all("a"):
+                  if l.get("href") is not None:
+                       href = l.get("href")
+                       if url.endswith("/"):
+                            url = url.rstrip("/")
+                       if len(href) > 0:
+                            if link_parse.scheme+"://"+link_parse.hostname is not href and not href.startswith("http"):
+                                 if not href.startswith("#") and not href.startswith("mailto"):
+                                      if "#" in href:
+                                           href = href.spit("#")[0]
+                                      if href.startswith("/"):
+                                            link_parse.scheme+"://"+link_parse.hostname+href
+                                      else:
+                                           href = url+"/"+href
+                                      if href is not list_link:
+                                           list_link.append(href)
+                                           self.all_link.append(href)
+                                           print(href)
+                                
+             return list_link
+        else:
+             return []
+
+if __name__ == "__main__":
+    a = Visite("https://google.com", "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0")
+    a.get_link()
+    a.all_link()
